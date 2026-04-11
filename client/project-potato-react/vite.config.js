d0fcbaee23127a938c6cbad83ee6436cf742203d
baker.lee <ljw722007@gmail.com>
2026-04-11 21:32:56 +0900

    to my wife

--- client/project-potato-react/vite.config.js
+++ client/project-potato-react/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5050",
+        target: process.env.VITE_PROXY_TARGET ?? "http://localhost:5050",
         changeOrigin: true,
       },
     },
